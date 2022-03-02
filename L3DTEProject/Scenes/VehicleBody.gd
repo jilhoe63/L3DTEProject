03f3d6412ba93a1852c5976ae54214ed5424e5b4
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2022-03-03 11:31:45 +1300

    This is the project file.

---
+++ L3DTEProject/Scenes/VehicleBody.gd
@@ -0,0 +1,23 @@
+extends VehicleBody
+	
+var horse_power = 200
+var accel_speed = 20
+
+var steer_angle = deg2rad(30)
+var steer_speed = 3
+
+var brake_power = 40
+var brake_speed = 40
+
+func _physics_process(delta):
+	var throt_input = -Input.get_action_strength("W")+Input.get_action_strength("S")
+	engine_force = lerp(engine_force.throt_input*horse_power.accel_speed*delta)
+
+	var steer_input = -Input.get_action_strength("D")+Input.get_action_strength("A")	
+	steering = lerp_angle(steering.steer_input*steer_angle.steer_speed*delta)
+	
+	var brake_input = Input.get_action_strength("SPACE")
+	brake = lerp(brake.brake_input*brake_power.brake_speed*delta)
+	
+func _ready():
+	pass # Replace with function body.
