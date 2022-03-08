2965b8fee9df863110aaf92e3b589cb85cf20f21
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2022-03-09 10:00:06 +1300

    Braking now works.

--- Scenes/VehicleBody.gd
+++ Scenes/VehicleBody.gd
@@ -1,23 +1,24 @@
 extends VehicleBody
 	
-var horse_power = 200
+var horse_power = 2000
 var accel_speed = 20
 
 var steer_angle = deg2rad(30)
 var steer_speed = 3
 
-var brake_power = 40
-var brake_speed = 40
+var brake_power = 10
+var brake_speed = 0
 
 func _physics_process(delta):
-	var throt_input = -Input.get_action_strength("W")+Input.get_action_strength("S")
-	engine_force = lerp(engine_force.throt_input*horse_power.accel_speed*delta)
+	var throt_input = Input.get_action_strength("W")-Input.get_action_strength("S")
+	engine_force = throt_input*horse_power
 
 	var steer_input = -Input.get_action_strength("D")+Input.get_action_strength("A")	
-	steering = lerp_angle(steering.steer_input*steer_angle.steer_speed*delta)
+	steering = lerp_angle(steering,steer_input*steer_angle,steer_speed*delta)
 	
 	var brake_input = Input.get_action_strength("SPACE")
-	brake = lerp(brake.brake_input*brake_power.brake_speed*delta)
+	brake = brake_power*brake_input
+	print(brake)
 	
 func _ready():
 	pass # Replace with function body.
