a5a1d305c6586de91bc5d19ed513c8f30a88971d
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2022-06-10 09:54:49 +1200

    Car working better

--- Car.gd
+++ Car.gd
@@ -1,15 +1,19 @@
 extends VehicleBody
 
 var max_rpm = 11000
-var max_torque = 400
+var max_torque = 300
+var brake_power = 4
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("D","A") * 0.4, 5 * delta)
 	var acceleration = Input.get_axis("S","W")
 	var rpm = $Rear_Left.get_rpm()
-	$Rear_Left.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	$Rear_Left.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm + 50)
 	rpm = $Rear_Right.get_rpm()
-	$Rear_Right.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	$Rear_Right.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm + 50)
 
+	var brake_input = Input.get_action_strength("SPACE")
+	brake = brake_power*brake_input
+		
 func _ready():
 	pass 
