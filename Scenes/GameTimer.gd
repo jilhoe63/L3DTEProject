f23ad8a97a46ee7472fdf298add76cc49042fd9d
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2022-09-02 11:19:31 +1200

    barriers finished and timer put in.

---
+++ Scenes/GameTimer.gd
@@ -0,0 +1,29 @@
+extends Label
+
+var time = 0
+var timer_on = false
+
+
+func _process(delta):
+	if(timer_on):
+		time += delta
+	
+	var mils = fmod(time,1)*1000
+	var secs = fmod(time,60)
+	var mins = fmod(time, 60*60) / 60
+	
+	var time_passed = "%02d : %02d : %03d" % [mins,secs,mils]
+	text = time_passed
+	
+	pass
+	
+func _ready():
+	pass
+
+
+
+func _on_Area_Timer_Start_body_entered(body):
+	if body.is_in_group("Player"):
+		timer_on = true
+		print("timer start")
+		pass
