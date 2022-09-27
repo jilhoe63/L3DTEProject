a1804890ab688fba1ddbeaebbe7a54395661d40a
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2022-09-28 10:54:03 +1300

    Game is tested some more.

--- Scenes/GameTimer.gd
+++ Scenes/GameTimer.gd
@@ -3,24 +3,18 @@ extends Label
 var time = 0
 var timer_on = false
 
-
 func _process(delta):
 	if(timer_on):
 		time += delta
-	
-	var mils = fmod(time,1)*100
-	var secs = fmod(time,120)
+	var mils = fmod(time,1)*1000
+	var secs = fmod(time,60)
 	var mins = fmod(time, 60*60) / 60
-	
 	var time_passed = "%02d : %02d : %03d" % [mins,secs,mils]
 	text = time_passed
-	
 	pass
 	
 func _ready():
 	pass
-
-
 
 func _on_Area_Timer_Start_body_entered(body):
 	if body.is_in_group("Player"):
@@ -28,11 +22,9 @@ func _on_Area_Timer_Start_body_entered(body):
 		print("timer start")
 		pass
 
-
 func _on_Area_Timer_Stop_body_entered(body):
 	if body.is_in_group("Player"):
 		timer_on = false
 		time = 0
 		print("timer start")
 		pass
-	pass # Replace with function body.
